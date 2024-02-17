436bca312af8ac779160861bb626e1cd91990771
fenixnec <taalaibekov003@gmail.com>
2024-02-17 14:00:54 +0600

    day00 DODE

---
+++ day_00/EditDistance/Program.cs
@@ -0,0 +1,83 @@
+﻿
+void Print() {
+    string filePath = "us_names.txt";
+    if (File.Exists(filePath))
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string? line;
+                Console.Write("Enter name: ");
+                string? name = Console.ReadLine();
+                bool find = false;
+                if (name != null)
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains(name)) 
+                    {
+                        Console.WriteLine($"Hello, {name}!");
+                        find = true;
+                        break;
+                    }
+                }
+
+                if(!find) 
+                {
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (name != null)
+                        if (LevenDis(name, line)==1) 
+                        {
+                            Console.WriteLine($"Did you mean “{line}”? Y/N");
+                            string? answ = Console.ReadLine();
+                            if(answ=="Y" || answ == "y") 
+                            {
+                                Console.WriteLine($"Hello, {line}!");
+                                find = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(!find) Console.WriteLine("Your name was not found.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+}
+
+Print();
+
+int LevenDis(string s, string t)
+{
+    int m = s.Length;
+    int n = t.Length;
+    int[,] d = new int[m + 1, n + 1];
+
+    // Инициализация первой строки и столбца
+    for (int i = 0; i <= m; i++)
+    {
+        d[i, 0] = i;
+    }
+    for (int j = 0; j <= n; j++)
+    {
+        d[0, j] = j;
+    }
+
+    // Вычисление расстояния Левенштейна
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 1; i <= m; i++)
+        {
+            int cost = (s[i - 1] == t[j - 1]) ? 0 : 1;
+            d[i, j] = Math.Min(
+                Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                d[i - 1, j - 1] + cost
+            );
+        }
+    }
+
+    return d[m, n];
+}
---
+++ day_00/LifeOnCredit/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Globalization;
+
+void calc() {
+    string[] args = Environment.GetCommandLineArgs();
+    if (args.Length != 4) {
+        System.Console.WriteLine("Something went wrong. Check your input and retry.");
+    }
+    else {
+        double sum = double.Parse(args[1]);
+        double rate = double.Parse(args[2]);
+        int term = int.Parse(args[3]);
+
+        DateTime currentDate = new DateTime(2021, 05, 01);
+        DateTime nextMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+        int dInYear, dInMonth, num = 1;
+        double i, princ, payment, interes, totalDebt = sum;
+        i = rate/12/100;
+        payment = sum * i * Math.Pow((1+i), term)/(Math.Pow(1+i, term)-1);
+        while (num <= term) {
+            dInYear = DateTime.IsLeapYear(currentDate.Year) ? 366 : 365;
+            dInMonth = DateTime.DaysInMonth(nextMonth.Year, nextMonth.Month);
+            nextMonth = nextMonth.AddMonths(1);
+            
+            interes = totalDebt * rate * dInMonth / (100*dInYear);
+            princ = payment-interes;
+            totalDebt = totalDebt-princ;
+            if (num==term) {
+                payment+=totalDebt;
+                totalDebt = 0;
+                princ = payment-interes;
+            }
+        
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            Console.Write($"{num} \t  ");
+            Console.Write($"{nextMonth.ToString("d", culture)} \t  ");
+            Console.Write($"{Math.Round(payment,2).ToString("N2",culture)}   \t  ");
+            Console.Write($"{Math.Round(princ,2).ToString("N2",culture)}  \t ");
+            Console.Write($"{Math.Round(interes,2).ToString("N2",culture)}  \t ");
+            Console.Write($"{Math.Round(totalDebt,2).ToString("N2",culture)} ");
+            Console.WriteLine("\t");
+            num++;
+        }
+    }
+
+}
+
+calc();
+
