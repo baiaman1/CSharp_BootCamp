111de6699ff8b27e4c2dbd66a1ff7daa88faa776
fenixnec <taalaibekov003@gmail.com>
2024-03-09 09:56:58 +0600

    team_00 init

---
+++ team_00/Program.cs
@@ -0,0 +1,20 @@
+﻿// decimal num = 0;
+// for (int i = 0; i < 10; i++)
+// {
+//     num += 0.1m;
+//     System.Console.WriteLine(i+1);
+// }
+// System.Console.WriteLine(num);
+
+// int a = 2, b = 3;
+// System.Console.WriteLine(a%b);
+
+var isA = false;
+var isB = false;
+var isC = true;
+
+if (isA || isB && isC) System.Console.WriteLine("one");
+if (isA | isB & isC) System.Console.WriteLine("two");
+
+string[] arr = new [4];
+
